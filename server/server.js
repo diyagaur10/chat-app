4e5571dfc5ffe1b7a2e0e56c1ed5c9e476ff0be7
diyagaur10 <diyagg59@gmail.com>
2025-07-13 17:31:52 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -52,6 +52,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log(`server is running on port ${PORT}`));
+}
+
+export default server;
