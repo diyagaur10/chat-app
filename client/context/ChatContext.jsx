32348ec8ac20ea70d054e6ed881f67cd96462d3b
diyagaur10 <diyagg59@gmail.com>
2025-07-13 17:19:42 +0530

    message feature

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,105 +1,92 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const ChatContext = createContext();
 
-export const ChatProvider = ({children}) =>{
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [unseenMessages, setUnseenMessages] = useState({});
+  const { socket, axios } = useContext(AuthContext);
 
-    const [messages, setMessages] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [unseenMessages, setUnseenMessages] = useState([]);
-    const {socket , axios}= useContext(AuthContext);
-
-    const getUsers = async () =>{
-        try{
-            const {data} = await axios.get("/api/messages/users");
-            if(data.success){
-                setUsers(data.users);
-                setUnseenMessages(data.unseenMessages);
-            }
-        }catch(error){
-            toast.error(error.message);
-        }
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("/api/messages/users");
+      if (data.success) {
+        setUsers(data.filteredUsers);
+        setUnseenMessages(data.unseenMessages);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    // fn to get mess for seleted user
-    const getMessages = async (userId) =>{
-        try{
-            const {data} = await axios.get(`/api/messages/${userId}`);
-            if(data.success){
-                setMessages(data.messages);
-            }
-        }catch(error){
-            toast.error(error.message);
-        }
+  const getMessages = async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/messages/${userId}`);
+      if (data.success) {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-// fn to send msg to seleted user\
-const sendMessage = async (messageData) =>{
-    try{
-        const {data} = await axios.post(`/api/messages/send${selectedUser._id}`, messageData);
-        if(data.success){
-            setMessages((prevMessages)=>[...prevMessages, data.newMessage])
-        }else{
-            toast.error(data.message);
-        }
-    }catch(error){
-        toast.error(error.message);
+  const sendMessage = async (messageData) => {
+    try {
+      const { data } = await axios.post(`/api/messages/send/${selectedUser._id}`, messageData);
+      if (data.success) {
+        setMessages((prev) => [...prev, data.newMessage]);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
-}
+  };
 
-// fn to subcribe to mgs for seleted user
+  const subscribeToMessages = () => {
+    if (!socket) return;
+    socket.on("newMessage", (newMessage) => {
+      if (selectedUser && newMessage.sender._id === selectedUser._id) {
+        newMessage.seen = true;
+        setMessages((prev) => [...prev, newMessage]);
+        axios.put(`/api/messages/mark/${newMessage._id}`);
+      } else {
+        setUnseenMessages((prev) => ({
+          ...prev,
+          [newMessage.senderId]: prev[newMessage.senderId] ? prev[newMessage.senderId] + 1 : 1,
+        }));
+      }
+    });
+  };
 
-const subscribeToMessages = () =>{
-    if(!socket) return;
-    socket.on("newMessage", (newMessage)=>{
-        if(selectedUser && newMessage.sender._id === selectedUser._id){
-            newMessage.seen = true;
-            setMessages((prevMessages)=>[...prevMessages, newMessage]);
-            axios.put(`/api/messages/mark/${newMessage._id}`);
-        }else{
-            setUnseenMessage((prevUnseenMessages)=>({...prevUnseenMessages, [newMessage.senderId]:
-                prevUnseenMessages[newMessage.senderId]? prevUnseenMessages[newMessage.senderId]+1:1
-                    
-                }))
-            
-        }
-    })
+  const unsubscribeFromMessages = () => {
+    if (socket) socket.off("newMessage");
+  };
 
-}
+  useEffect(() => {
+    subscribeToMessages();
+    return () => unsubscribeFromMessages();
+  }, [socket, selectedUser]);
 
-// fn to unsubcribe from messages
+  const value = {
+    messages,
+    users,
+    selectedUser,
+    unseenMessages,
+    getUsers,
+    
+    setSelectedUser,
+    setUnseenMessages,
+    getMessages,
+    sendMessage,
+  };
 
-const unsubscribeFromMessages = () =>{
-    if(socket)  socket.off("newMessage");
-}
-
-    useEffect(()=>{
-        subscribeToMessages();
-        return ()=>unsubscribeFromMessages();
-            
-        }, [socket, selectedUser])
-
-
-    const value = {
-        messages,
-        users,
-        selectedUser,
-        unseenMessages,
-        getUsers,
-        setMessages,
-        setSelectedUser,
-        setUnseenMessages,
-        getMessages,
-        sendMessage
-    }
-    return(
-        <ChatContext.Provider value={value}>
-            {children}
-        </ChatContext.Provider>
-    );
-}
+  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+};
 
 export default ChatProvider;
-
-
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,20 @@
 
-import React, { useState } from 'react'
+import React, { useState , useContext} from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatCointainer from '../components/ChatCointainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
+
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false)
+  const { selectedUser } = useContext(ChatContext)
   return (
     <div className='border w-full h-screen border-white sm:px-[15%] sm:py-[5%]'>
       <div className={`backdrop-blur-xl border-2 border-blue-300
          rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
-        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-        <ChatCointainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <Sidebar />
+        <ChatCointainer />
+        <RightSidebar />
       </div>
     </div>
   )
